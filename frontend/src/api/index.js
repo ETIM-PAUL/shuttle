189c54fec47c8172b024bc06bcfcc17d86025e7f
undefined <etimpaul22@gmail.com>
2025-10-13 00:20:28 +0100

    get btc price

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -20,6 +20,28 @@ export const getBtcBalance = async (address) => {
       }
 }
 
+export const getBtcPrice = async () => {
+    try {
+        const response = await fetch(`/api/v1/bitcoin/price`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': import.meta.env.VITE_XVERSE_API_Key
+          },
+        });
+    
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+    
+        const data = await response.json();
+        return data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+      }
+}
+
 export const getVesuGenesisDetails = async () => {
     try {
         const response = await fetch(`https://dev.api.vesu.xyz/pools/566154675190438152544449762131613456939576463701265245209877893089848934391`, {
